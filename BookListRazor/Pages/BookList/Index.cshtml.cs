5f05af2a27030254825219c8070e67a2ae440a90
farzanahmad863 <farzan.ahmad863@yahoo.com>
2021-02-26 09:52:56 +0100

    Added functionality to delete button.

--- BookListRazor/Pages/BookList/Index.cshtml.cs
+++ BookListRazor/Pages/BookList/Index.cshtml.cs
@@ -17,9 +17,21 @@ namespace BookListRazor.Pages.BookList
             _db = db;
         }
         public IEnumerable <Book> Books { get; set; }
-        public async Task OnGetAsync()
+        public async Task OnGet()
         {
             Books = await _db.Book.ToListAsync();
         }
+        public async Task<IActionResult> OnPostDelete(int id)
+        {
+            Book book = await _db.Book.FindAsync(id);
+            if (book == null)
+            {
+                return NotFound();
+            }
+            _db.Book.Remove(book);
+            await _db.SaveChangesAsync();
+            return RedirectToPage("Index");
+        }
+
     }
 }
